f8c7c466e636fc099837e4b10e307fb7ccf5b806
Mohamed Salah <salahineo.personal@gmail.com>
2021-01-28 19:44:21 +0200

    Add Dynamic Footer Year Copyright

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ $(function () {
     upperNavHeight = $(".upperNav").innerHeight(),
     mainNavHeight = $(".navbar").innerHeight();
   $(".carousel").height(windowHeight - (upperNavHeight + mainNavHeight));
+
+  // New date Object
+  let currentDate = new Date();
+  // Get Copyright Year Span
+  document.getElementById("footer-copyright-year").innerHTML = String(currentDate.getFullYear());
 });
